6b93b361d1c02fd73a59f68b5f59a00c0f834738
ducc <aaronhwang22@gmail.com>
2025-03-24 03:10:40 -0400

    Implemented: helper func for number of bytes, inserting into a leaf, appending across a range, splitting node's keys, and more.

--- pkg/btree/node.go
+++ pkg/btree/node.go
@@ -1,6 +1,7 @@
 package btree
 
 import (
+	"bytes"
 	"encoding/binary"
 	"errors"
 )
@@ -17,21 +18,31 @@ const (
 	HEADER_SIZE = 4
 )
 
-type Node struct {
-	keys     [][]byte
-	vals     [][]byte
-	children []*Node
-}
+// Pseudocode for a btree. However, writing to a direct bytes slice is much faster.
+// type Node struct {
+// 	// Every node has these.
+// 	keys     [][]byte
+// 	// Only leaf nodes should have these
+// 	vals     [][]byte
+// 	// Only internal nodes should have these (non leaf nodes)
+// 	children []*Node
+// }
 
 // For decoding nodes real fast
 type BNode []byte
 
+// Return the
 func (node BNode) btype() uint16 {
 	return binary.LittleEndian.Uint16(node[0:2])
 }
 
 func (node BNode) nkeys() uint16 {
 	return binary.LittleEndian.Uint16(node[2:4])
+}
+
+func (node BNode) nbytes() uint16 {
+	kvpos, _ := node.kvPos(node.nkeys())
+	return kvpos
 }
 
 func (node BNode) setHeader(btype uint16, nkeys uint16) {
@@ -113,10 +124,101 @@ func (node BNode) isValidIndex(index uint16) bool {
 	return index < node.nkeys()
 }
 
-func encode(node *Node) []byte {
-	return []byte{}
+// Insert a new key at position index
+func leafInsert(next BNode, old BNode, index uint16, key []byte, val []byte) {
+	next.setHeader(LEAF, old.nkeys()+1)
+	nodeAppendAcrossRange(next, old, 0, 0, index)
 }
 
-func decode(page []byte) *Node {
-	return &Node{}
+// Append n keys to next from old,
+func nodeAppendAcrossRange(next BNode, old BNode, dest uint16, src uint16, n uint16) {
+	for i := uint16(0); i < n; i++ {
+		dst, source := dest+i, src+i
+		// Placeholder; not sure what to do with error for now; probably propagate?
+		oldPtr, _ := old.getPtr(source)
+		oldKey, _ := old.getKey(source)
+		oldVal, _ := old.getValue(source)
+		nodeAppendKeyVal(next, dst, oldPtr, oldKey, oldVal)
+	}
+}
+
+func nodeAppendKeyVal(next BNode, destination uint16, ptr uint64, key []byte, val []byte) {
+
+}
+
+// Update the given new leaf to
+func updateLeaf(next, old BNode, index uint16, key, val []byte) {
+	next.setHeader(LEAF, old.nkeys())
+	nodeAppendAcrossRange(next, old, 0, 0, index)
+	nodeAppendKeyVal(next, index, 0, key, val)
+	nodeAppendAcrossRange(next, old, index+1, index+1, old.nkeys()-(index+1))
+}
+
+// Find the last position less than or equal to the given key; used to maintain sorted order when updating keys
+func nodeLookupLE(node BNode, key []byte) uint16 {
+	nkeys := node.nkeys()
+	var i uint16
+	// TODO: Change to binary search eventually. Probably not a huge issue considering would need thousand+ keys to make diff
+	for i = 0; i < nkeys; i++ {
+		// For now just discard any errors, future me problem
+		compkey, _ := node.getKey(i)
+		cmp := bytes.Compare(compkey, key)
+		// Equal
+		if cmp == 0 {
+			return i
+		}
+		// key is bigger than i
+		if cmp > 0 {
+			return i - 1
+		}
+	}
+
+	// We iterated through every position, means key is greater than every other key
+	return i - 1
+}
+
+// Split a node's keys in half. For writing to disk, make sure a node still fits in one page. Split them among left and right respectively.
+func nodeSplitInHalf(left, right, old BNode) error {
+	nkeys := old.nkeys()
+	if nkeys < 2 {
+		return errors.New("placeholder error for when splitting a node in half; number of keys in old less than 2")
+	}
+
+	// Try to slice the keys in half, stuff it into a page size
+	// If we exceed page size, keep shrinking until we don't
+	numleft := nkeys / 2
+	left_bytes := func() uint16 {
+		return 4 + 8*numleft + 2*numleft + old.getOffset(numleft)
+	}
+
+	for left_bytes() > BTREE_PAGE_SIZE_BYTES {
+		numleft--
+	}
+
+	if numleft < 1 {
+		// Placeholderish
+		return errors.New("error: number of keys on the left after split attempt was 0.")
+	}
+
+	// Do the same for the right. Start from where numleft left off.
+	right_bytes := func() uint16 {
+		return old.nbytes() - left_bytes()*4
+	}
+
+	for right_bytes() > BTREE_PAGE_SIZE_BYTES {
+		numleft++
+	}
+
+	if numleft >= nkeys {
+		return errors.New("error when shifting keys")
+	}
+
+	numRight := nkeys - numleft
+
+	left.setHeader(old.btype(), numleft)
+	right.setHeader(old.btype(), numRight)
+	nodeAppendAcrossRange(left, old, 0, 0, numleft)
+	nodeAppendAcrossRange(right, old, 0, 0, numRight)
+
+	return nil
 }
