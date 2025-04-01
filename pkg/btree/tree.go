072ffa11004efd2e65d55de9702af62ff3102e2d
ducc <aaronhwang22@gmail.com>
2025-03-31 23:28:37 -0400

    new btree methods

---
+++ pkg/btree/tree.go
@@ -0,0 +1,36 @@
+package btree
+
+import "bytes"
+
+type BTree struct {
+	root uint64
+	// Read data from a given page number
+	get func(uint64) []byte
+	// Create a new page with given data
+	create func([]byte) uint64
+	// Delete/dealloc the given page number
+	del func(uint64)
+}
+
+func treeInsert(tree *BTree, node BNode, key, val []byte) BNode {
+	next := BNode(make([]byte, 2*BTREE_PAGE_SIZE_BYTES))
+
+	index := nodeLookupLE(node, key)
+	switch node.btype() {
+	case LEAF:
+		// TODO: Error handling
+		key, _ := node.getKey(index)
+		if bytes.Equal(key, key) {
+			leafUpdate(next, node, index, key, val)
+		} else {
+			leafInsert(next, node, index+1, key, val)
+		}
+	case NODE: // Internal node, walk into the child node
+		// TODO: Error handling
+		// Recursively insert into child node
+		kptr, _ := node.getPtr(index)
+		knode := treeInsert(tree, tree.get(kptr), key, val)
+		// After we insert, split
+
+	}
+}
